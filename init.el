a92d6c6a5b553c518377525387bacdd780abe9d3
Mykhas Kobernyk <m.kobernyk@gmail.com>
2019-01-15 16:21:51 +0200

    [init.el] Latest (and dirty) Emacs config

--- init.el
+++ init.el
@@ -18,28 +18,126 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-(add-hook 'org-mode-hook (lambda () (setq truncate-lines t)))
 (add-hook 'org-mode-hook 'org-indent-mode)
-(add-hook 'org-mode-hook 'toggle-truncate-lines)
+(add-hook 'org-mode-hook 'auto-fill-mode)
+(setq org-startup-truncated t)
+(setq org-todo-keywords
+  '((sequence "TODO"
+      "MAYBE"
+      "NEXT"
+      "STARTED"
+      "WAITING"
+      "DELEGATED"
+      "|"
+      "DONE"
+      "DEFERRED"
+      "CANCELLED")))
+(require 'org-trello)
+(require 'org-journal)
+(setq org-agenda-files '("~/Portknox/Org"))
+(setq org-journal-enable-agenda-integration t)
+(setq org-journal-dir "~/Portknox/Org/journal")
+(setq org-journal-file-format "%Y%m%d.org")
+(setq org-journal-carryover-items "TODO=\"TODO\"|TODO=\"STARTED\"|TODO=\"NEXT\"|TODO=\"MAYBE\"|TODO=\"WAITING\"")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
+ '(org-trello-files
+   (quote
+    ("/home/mykhas/Documents/Dropbox/org/chgk/trivia.org")) nil (org-trello))
+ '(package-selected-packages
+   (quote
+    (jscs use-package elfeed elfeed-org pdf-tools pamparam ess org-pomodoro tss mastodon org-trello nov multi-term tide ng2-mode typescript-mode web-mode flycheck xref-js2 js2-refactor js2-mode exec-path-from-shell flymake-json yafolding json-mode editorconfig)))
+ '(send-mail-function (quote smtpmail-send-it)))
+
+;; NOV
+(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
+(setq nov-text-width 50)
 
 ;; TERM
 (require 'multi-term)
 (setq multi-term-program "/bin/bash")
 
+;; MASTODON
+(setq mastodon-instance-url "https://wandering.shop")
+
+;; RSS
+(require 'use-package)
+(use-package elfeed-org
+  :ensure t
+  :config
+  (elfeed-org)
+  (setq rmh-elfeed-org-files (list "~/Portknox/Org/rss.org")))
+;;shortcut functions
+(defun bjm/elfeed-show-all ()
+  (interactive)
+  (bookmark-maybe-load-default-file)
+  (bookmark-jump "all"))
+(defun bjm/elfeed-show-schdk ()
+  (interactive)
+  (bookmark-maybe-load-default-file)
+  (bookmark-jump "schdk"))
+(defun bjm/elfeed-show-news ()
+  (interactive)
+  (bookmark-maybe-load-default-file)
+  (bookmark-jump "news"))
+
+;;functions to support syncing .elfeed between machines
+;;makes sure elfeed reads index from disk before launching
+(defun bjm/elfeed-load-db-and-open ()
+  "Wrapper to load the elfeed db from disk before opening"
+  (interactive)
+  (elfeed-db-load)
+  (elfeed)
+  (elfeed-search-update--force))
+
+;;write to disk when quiting
+(defun bjm/elfeed-save-db-and-bury ()
+  "Wrapper to save the elfeed db to disk before burying buffer"
+  (interactive)
+  (elfeed-db-save)
+  (quit-window))
+
+(require 'use-package)
+(use-package elfeed
+  :ensure t
+  :bind (:map elfeed-search-mode-map
+              ("A" . bjm/elfeed-show-all)
+              ("S" . bjm/elfeed-show-schdk)
+              ("N" . bjm/elfeed-show-news)
+              ("q" . bjm/elfeed-save-db-and-bury)))
+
+
 ;; BASIC THEME AND SETTINGS
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
 (set-frame-font "Fira Mono")
+(set-face-attribute 'default nil :height 104)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (setq-default indent-tabs-mode nil)
 (setq backup-directory-alist `(("." . "~/.emacs-saves")))
+(defvar autosave-dir (concat "~/.emacs-saves" "/"))
+(make-directory autosave-dir t)
+(setq auto-save-file-name-transforms
+      `(("\\(?:[^/]*/\\)*\\(.*\\)" ,(concat autosave-dir "\\1") t)))
 
-;; FUNCTIONS
-(defun dear-diary ()
-  "This function can be used to create an org file with today as it's file name."
-  (interactive)
-  (find-file (concat "~/Documents/Dropbox/org/" (format-time-string "%Y-%W.org"))))
+;; MAIL
+(require 'mu4e)
+(setq mail-user-agent 'mu4e-user-agent)
+
+;; default
+(setq mu4e-maildir "~/Maildir")
+
+(setq mu4e-maildir-shortcuts
+      '( ("/Gmail_Offlineimap/INBOX"        . ?g)))
+
+;; allow for updating mail using 'U' in the main view:
+(setq mu4e-get-mail-command "offlineimap")
 
 ;; BASHRC
 (exec-path-from-shell-initialize)
@@ -124,17 +222,10 @@
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 ;; CUSTOM
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (multi-term tide ng2-mode typescript-mode web-mode flycheck xref-js2 js2-refactor js2-mode exec-path-from-shell flymake-json yafolding json-mode editorconfig))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(variable-pitch ((t (:family "Fira Sans")))))
